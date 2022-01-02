5cdf68884ce0ee03982bc63826e25c831d317636
kota <kokota.ohara@gmail.com>
2022-01-03 01:32:51 +0900

    amplify add function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "sample": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
